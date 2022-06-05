3c731ae077ae92e9b2e6a0fa83680aa13354dc06
jrodr117 <jrodr113@msudenver.edu>
2022-06-05 16:14:23 -0600

    leveraged body-parser module to make a post request with user data and access data  in my server

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const request = require('request');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res) => {
+	var firstName = req.body.fName;
+	var lastName = req.body.lName;
+	var email = req.body.email;
+
+	console.log(firstName, lastName, email);
 });
 
 app.listen(3000, () => {
